332ed839efe8b258bdd1216bb26beeda80f65f41
Eve Fon Wu <evefonwu@gmail.com>
2016-12-23 19:45:25 -0500

    redirect to dashboard on saved new expense

--- reactjsapp/reducers/saved.js
+++ reactjsapp/reducers/saved.js
@@ -4,6 +4,7 @@ const saved = (state = false, action) => {
   switch (action.type) {
     case types.ADD_EXPENSE:
       return true;
+    case types.RECEIVE_EXPENSES:
     case types.ERROR_ADD_EXPENSE:
       return false;
     default:
