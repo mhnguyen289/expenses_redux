f3b2b0af83806a3cae9a70474427fbf2c2bd4dbd
Eve Fon Wu <evefonwu@gmail.com>
2016-12-29 16:36:06 -0500

    test split utility functions

--- reactjsapp/utils/split.spec.js
+++ reactjsapp/utils/split.spec.js
@@ -2,35 +2,36 @@ import expect from 'expect';
 import * as splitUtil from './split';
 
 describe('methods for splitting expenses among members', () => {
-  const amount = '100.00';
-
   it('should handle splitting expenses equally', () => {
+    const amount = '360.00';
     const friends = [
       { id: 1, owed: '' },
       { id: 2, owed: '' },
     ];
     const actual = splitUtil.splitEqually(friends, amount);
     const expected = {
-      1: '50',
-      2: '50',
+      1: '180',
+      2: '180',
     };
     expect(actual).toEqual(expected);
   });
 
   it('should handle splitting expenses by percent', () => {
+    const amount = '80.00';
     const friends = [
       { id: 1, owed: '60' },
       { id: 2, owed: '40' },
     ];
     const actual = splitUtil.splitByPercent(friends, amount);
     const expected = {
-      1: '60',
-      2: '40',
+      1: '48',
+      2: '32',
     };
     expect(actual).toEqual(expected);
   });
 
   it('should handle splitting expenses by exact amount', () => {
+    const amount = '100.00';
     const friends = [
       { id: 1, owed: '55.63' },
       { id: 2, owed: '44.37' },
