680079f08f6a52bc3c359307da994eae46bbe91e
Eve Fon Wu <evefonwu@gmail.com>
2016-12-29 22:59:47 -0500

    organize code

--- reactjsapp/configureStore.spec.js
+++ reactjsapp/configureStore.spec.js
@@ -3,6 +3,20 @@ import expect from 'expect';
 import * as types from './constants/action_types';
 
 describe('store', () => {
+  it('should handle initial state', () => {
+    const store = configureStore();
+    const actual = store.getState();
+    const expected = {
+      authenticated: false,
+      debts: { byId: {}, allIds: [] },
+      expenses: { byId: {}, allIds: [] },
+      friends: { byId: {}, allIds: [] },
+      message: '',
+      savedExpense: false,
+    };
+    expect(actual).toEqual(expected);
+  });
+
   it('should dispatch actions', () => {
     const store = configureStore();
     store.dispatch({
@@ -20,4 +34,5 @@ describe('store', () => {
     };
     expect(actual).toEqual(expected);
   });
-})
+
+});
