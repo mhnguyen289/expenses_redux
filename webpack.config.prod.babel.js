736a6fc198f5eadae625f948ec851af81ad79bda
Eve Fon Wu <evefonwu@gmail.com>
2016-12-31 15:03:44 -0500

    organize code

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
   ],
   module: {
     loaders: [
