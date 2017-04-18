f033e7368157640842737bac847e9e2dd3391e43
evefonwu <evefonwu@gmail.com>
2017-04-17 22:53:23 -0400

    add postcss loaders and plugins

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -11,6 +11,14 @@ module.exports = {
       { test: /\.js$/,
         loaders: ['babel'], exclude: /node_modules/, include: __dirname,
       },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader?importLoaders=1',
+          'postcss-loader'
+        ],
+      },
     ],
   },
   devtool: 'source-map',
