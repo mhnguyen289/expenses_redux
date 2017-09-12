5c585fffe93ab80fa3695a45903490fdfda5e3bd
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 00:01:19 -0700

    passing user model test for unique emails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,45 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
 
-    it 'requires a email and password when created'
+   it 'requires an email and password upon creation' do 
+      user = build(:user, email: nil, password: nil)
 
-    it 'requires that an email is unique'
-    
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank"
+      ])
+    end
+
+   it 'requires that an email is unique' do 
+      create(:user)
+      user = build(:user)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank"
+      ])
+    end
+it 'requires that an email is valid (contains an @ symbol and a (.com, .org, etc...)' do 
+      user1 = build(:user, email: 'minh.com')
+      user2 = build(:user, email: 'minh@something')
+      user3 = build(:user, email: 'minh')
+
+      expect(user1.valid?).to equal(false)
+      expect(user1.errors.full_messages).to eq([
+        "Email is invalid"
+      ])
+      expect(user2.valid?).to equal(false)
+      expect(user3.valid?).to equal(false)
+    end
+
+  end
+
+  describe 'on save' do
+
+    it 'hashes a password'
+
   end
 
   describe 'relationships' do
