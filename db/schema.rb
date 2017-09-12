8dceb9f9b455bba602cd43dbdb3a56bc58fde503
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 03:00:53 -0700

    created line_item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912084413) do
+ActiveRecord::Schema.define(version: 20170912100004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20170912084413) do
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["item_id"], name: "index_line_items_on_item_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -48,4 +58,6 @@ ActiveRecord::Schema.define(version: 20170912084413) do
 
   add_foreign_key "carts", "users"
   add_foreign_key "items", "categories"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "items"
 end
