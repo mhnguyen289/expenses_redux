98f6f9d2c0b8f00a59cddc19871468c42f3f45ac
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 01:15:38 -0700

    instantiate cart model

--- db/migrate/20170912080822_create_carts.rb
+++ db/migrate/20170912080822_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration[5.1]
   def change
     create_table :carts do |t|
       t.references :user, foreign_key: true
-      t.string :status
+      t.string :status, default: "Active"
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170912080822) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "status"
+    t.string "status", default: "Active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
