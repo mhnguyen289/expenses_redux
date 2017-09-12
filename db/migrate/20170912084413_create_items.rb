15c27e527d5ec30e69393f5224765b929aa97569
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 01:49:10 -0700

    create item model

---
+++ db/migrate/20170912084413_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.integer :inventory
+      t.decimal :price
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :item do
+    title "MyString"
+    inventory 1
+    price "9.99"
+    category nil
+  end
+end
