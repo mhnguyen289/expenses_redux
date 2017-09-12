070be8cd965e4fb0a05707989466134145b26a7a
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 01:49:20 -0700

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912080822) do
+ActiveRecord::Schema.define(version: 20170912084413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(version: 20170912080822) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.integer "inventory"
+    t.decimal "price"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -31,4 +47,5 @@ ActiveRecord::Schema.define(version: 20170912080822) do
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "items", "categories"
 end
