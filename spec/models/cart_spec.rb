d01354abe4b0efc54a3e4db6b3c0a4970819b996
Minh Nguyen <mhnguyen289@gmail.com>
2017-09-12 01:38:16 -0700

    user now has one cart and its destroyed if deleted

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  
+  describe 'validations' do 
+
+    it 'requires a user upon creation' do 
+      cart = build(:cart, user: nil)
+
+      expect(cart.valid?).to eq(false)
+      expect(cart.errors.full_messages).to eq(["User must exist"])
+    end
+  end 
+
+  describe 'relationships' do 
+
+    it 'has many line items that are destroyed upon deletion of cart' do 
+      cart = create(:cart)
+      item = create(:item)
+      line_item = cart.line_items.create(quantity: 1, item: item)
+
+      expect(cart.line_items.first.id).not_to eq(nil)
+
+      cart.destroy
+      line_item = LineItem.find_by(id: line_item.id)
+      
+      expect(line_item).to eq(nil)
+    end
+
+    it 'has many items through line items' do 
+      cart = create(:cart)
+      item = create(:item)
+      line_item = cart.line_items.create(quantity: 1, item: item)
+
+      expect(cart.items.count).to eq(1)
+    end
+
+    it 'belongs to a user' do 
+      cart = create(:cart)
+      
+      expect(cart.user.email).to eq("avi@flatironschool.com")
+    end
+  end
+  end
